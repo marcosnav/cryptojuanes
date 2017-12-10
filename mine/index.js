5a932eedc1d2fa750498fb55d80a58bb2ac5cf3e
Marcos Navarro <marcos@aftertype.com>
2017-12-09 20:36:14 -0600

    progress 8 30pm

--- mine/index.js
+++ mine/index.js
@@ -1,8 +1,9 @@
-
+const uuid = require('uuid/v4')
+const pryjs = require('pryjs')
 const _ = require('lodash')
 const max_size = 10e5;
 const merkle = require('merkle');
-const fee_rate = 1;//14000;
+const fee_rate = 14000;
 const api = require('../api')
 let reward_network;
 const public_key_compressed = '2bfcc6808f5679ee357590f8ff126ec01cfc061a';
@@ -22,6 +23,14 @@ const get_txn_hashes = (pool_transactions) => {
   });
 }
 
+function toHex(str) {
+  var hex = '';
+  for(var i=0;i<str.length;i++) {
+    hex += ''+str.charCodeAt(i).toString(16);
+  }
+  return hex;
+}
+
 Mine.prototype.add_transaction = function (transaction) {
   let container = this.calculate_fee(transaction);
   if (!container.isValid)
@@ -32,13 +41,15 @@ Mine.prototype.add_transaction = function (transaction) {
   if ((total_size + container.transaction.size) < max_size) {
     this.pool_transactions.push(container.transaction);
   } else {
-    this.merkle_hash = this.generate_merkle_hash(this.pool_transactions);
+    // this.merkle_hash = this.generate_merkle_hash(this.pool_transactions);
     this.build_block();
   }
 };
 
 Mine.prototype.generate_merkle_hash = function (pool_transactions) {
   const hashes = get_txn_hashes(pool_transactions)
+
+  console.log(hashes)
 
   const tree_parse = (hashes) => {
     let temp_tree = [];
@@ -70,40 +81,47 @@ Mine.prototype.generate_merkle_hash = function (pool_transactions) {
 };
 
 Mine.prototype.build_block = function () {
+  let transaction_fees = _.sumBy(this.pool_transactions, (item) => {
+    return item.fee;
+  });
 
-  let stamp = "cryptojuanes baby !!!"
-  let message = "1|" + this.prev_block.hash + "|" + this.merkle_hash + "|" +
-    this.prev_block.target + "|" + stamp
+  let network_reward = this.calculate_reward_network()
+  let reward = network_reward + transaction_fees;
 
-  var go_process = spawn('./bin/powHash', ['inc', message, this.prev_block.target]);
+  let coin_base_transaction = this.generate_coinbase_transaction(network_reward);
+
+  this.merkle_hash = this.generate_merkle_hash(this.pool_transactions);
+  let stamp = uuid()
+  // let message = "1|" + this.prev_block.hash + "|" + coin_base_transaction.hash + "|" + this.prev_block.target + "|" + stamp
+  let message = "1|" + this.prev_block.hash + "|" + this.merkle_hash + "|" + this.prev_block.target + "|" + stamp
+
+  var go_process = spawn('./bin/powHash', ['0', message, this.prev_block.target]);
 
   go_process.stdout.on('data', (data) => {
     let hash_block = data.toString().split('|')[0];
     let nonce = data.toString().split('|')[1];
 
-    let transaction_fees = _.sumBy(this.pool_transactions, (item) => {
-      return item.fee;
-    });
 
+    console.log("GO: ", data.toString())
 
-    let reward = this.calculate_reward_network() + transaction_fees;
-
-
-    let coin_base_transaction = this.generate_coinbase_transaction(reward);
 
     this.pool_transactions.push(coin_base_transaction);
+
+    // eval(pryjs.it)
 
     let payload = {
       prev_block_hash: this.prev_block.hash,
       hash: hash_block,
       height: this.prev_block.height + 1,
       message: stamp,
-      merkle_hash: this.merkle_hash,
+      // merkle_root: coin_base_transaction.hash,
+      merkle_root: this.merkle_hash,
+      // transactions: [coin_base_transaction],
       transactions: this.pool_transactions,
       reward: reward,	//amount	Total reward of the block (network reward + transaction fees)
       nonce: nonce,
       nickname: 'cryptojuanes',
-      target: this.prev_block.target,
+      used_target: this.prev_block.target,
       created_at: Date.now()
     }
     api.postBlock(payload).then((response) => {
@@ -111,6 +129,7 @@ Mine.prototype.build_block = function () {
       this.initialize();
     }).catch((err) => {
       console.log(err.response.data);
+      this.initialize();
     })
 
   });
@@ -146,6 +165,8 @@ Mine.prototype.initialize = function () {
       this.height = this.prev_block.height;
 
       pool_transactions.forEach((transaction) => {
+        if (this.pool_transactions.length == 20)
+          return
         let container = this.calculate_fee(transaction);
         if (container.isValid) {
           this.pool_transactions.push(container.transaction);
@@ -173,46 +194,42 @@ Mine.prototype.calculate_fee = function (origin_transaction) {
 Mine.prototype.generate_coinbase_transaction = function (reward) {
 
   let prev_hash = "0000000000000000000000000000000000000000000000000000000000000000";
-  let script_sig = "cryptojuanes baby!!!";
+  let script_sig = "0123456789bbbcdef";
   let vout = "-1";
 
-  let input_payload = Buffer.concat(
-    [this.text2Binary(prev_hash, 'hex'),
-    this.text2Binary(script_sig),
-    this.text2Binary(vout)]
-  )
-
+  let input_payload = Buffer.concat([
+    new Buffer(prev_hash, 'hex'),
+    new Buffer(script_sig, 'hex'),
+    new Buffer(vout)
+  ])
 
   let value = reward;
-  output_payload = Buffer.concat(
-    [
-      this.text2Binary(value.toString()),
-      this.text2Binary(public_key_compressed.length.toString()),
-      this.text2Binary(public_key_compressed.toString(), 'hex')
-    ]
-  )
+  output_payload = Buffer.concat([
+    new Buffer(value.toString()),
+    new Buffer(new Buffer(public_key_compressed, 'hex').length.toString()),
+    new Buffer(public_key_compressed, 'hex')
+  ])
 
+  let transaction = Buffer.concat([
+    new Buffer("1"),
+    new Buffer("1"),
+    input_payload,
+    new Buffer("1"),
+    output_payload,
+    new Buffer("0")
+  ])
 
-  transaction = Buffer.concat(
-    [
-      this.text2Binary("1"),
-      this.text2Binary("1"),
-      this.text2Binary(input_payload, "hex"),
-      this.text2Binary("1"),
-      this.text2Binary(output_payload, "hex"),
-      this.text2Binary("0")
-    ]
-  )
+  hash = sha256.x2(transaction).split('').reverse().join('');
 
-  hash = sha256.x2(transaction.toString('hex')).split('').reverse().join('');
   console.log(hash)
-  transaction = {
+
+  const transaction_ready = {
     hash: hash,
     inputs: [
       {
         prev_hash: "0000000000000000000000000000000000000000000000000000000000000000",
         vout: -1,
-        script_sig: "cryptojuanes baby!!!"
+        script_sig,
       }
     ],
     outputs: [
@@ -223,7 +240,7 @@ Mine.prototype.generate_coinbase_transaction = function (reward) {
     ]
   }
 
-  return transaction;
+  return transaction_ready;
 
 }
 
